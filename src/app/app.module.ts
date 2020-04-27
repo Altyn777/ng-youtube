626ad79020eeeb84d1e3ca7a2b347edc445ccc81
Altyn <darzeeia@gmail.com>
2020-04-27 14:03:30 +0300

    step2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
@@ -9,7 +10,8 @@ import { TodosComponent } from './todos/todos.component';
     AppComponent, TodosComponent
   ],
   imports: [ // регистрируем другие модули
-    BrowserModule // позволяет работать с angular в браузере
+    BrowserModule, // позволяет работать с angular в браузере
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent] // в поле передается компонент
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TodosService} from "../shared/todos.service";
+// import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todos', // позволяет использовать app-todos в html
@@ -7,9 +8,17 @@ import {TodosService} from "../shared/todos.service";
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit { // OnInit - interface for lifecycle hooks
+
+  public loading: boolean = true
+
   constructor(public todosService: TodosService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // компонент стартует
+    this.todosService.fetchTodos()
+      // .pipe(delay(2000))
+      .subscribe(() => {
+      this.loading = false
+    })
   }
 
   onChange(id: number) {
