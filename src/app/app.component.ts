cde54224f5c0fbc7d45840591144c19c6cb97af1
Altyn <darzeeia@gmail.com>
2020-04-30 10:59:30 +0300

    card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({ // декоратор
-  selector: 'app-root', // поле selector
-  templateUrl: './app.component.html', // в данном файле хранится шаблон этого компонента
-  styleUrls: ['./app.component.scss'] // стили
+@Component({
+  selector: 'app-root', // в нем сточка app-root
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
-  appTitle = 'Angular todo application' // поле передается в шаблон html
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { TodosComponent } from './todos/todos.component';
-import { TodoFormComponent } from './todo-form/todo-form.component';
-import {TodosFilterPipe} from "./shared/todos-filter.pipe";
+import {CardComponent } from './card/card.component'
 
-@NgModule({ // декоратор
-  declarations: [ // регистрируем компоненты
+@NgModule({ // декоратор, название сущности (синтаксис ts; это f, которая оборачивает класс, делая его модулем)
+  declarations: [ // модуль регистрирует компоненты
     AppComponent,
-    TodosComponent,
-    TodoFormComponent,
-    TodosFilterPipe,
+    CardComponent,
   ],
-  imports: [ // регистрируем другие модули
-    BrowserModule, // позволяет работать с angular в браузере
-    HttpClientModule,
-    FormsModule,
+  imports: [ // ...и другие модули
+    BrowserModule // этот позволяет работать в браузере
   ],
-  providers: [],
-  bootstrap: [AppComponent] // в поле передается компонент
+  providers: [], // тут хранятся различные сервисы, интерсептеры
+  bootstrap: [AppComponent] // все приложение стартует с этого компонента
 })
 export class AppModule { }
